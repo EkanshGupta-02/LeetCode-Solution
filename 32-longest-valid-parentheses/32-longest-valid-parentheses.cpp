2124ad7728b4207cf9a15daa7523bcb126fa1281
EkanshGupta-02 <76432963+EkanshGupta-02@users.noreply.github.com>
2022-05-24 22:49:56 +0530

    Time: 9 ms (28.15%), Space: 7.3 MB (67.93%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,64 +1,64 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-//         int maxi=0;
+        int maxi=0;
         
-//         stack<int> st;
-//         st.push(-1);
+        stack<int> st;
+        st.push(-1);
         
-//         for(int i=0;i<s.size();i++){
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                st.push(i);
+            }
+            else{
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }
+                else{
+                    maxi=max(maxi,i-st.top());
+                }
+            }
+        }
+        
+        return maxi;
+        
+//         int maxians=0;
+//         int left=0,right=0;
+        
+//         for(int i=0;i<s.length();i++){
 //             if(s[i]=='('){
-//                 st.push(i);
+//                 left++;
 //             }
 //             else{
-//                 st.pop();
-//                 if(st.empty()){
-//                     st.push(i);
-//                 }
-//                 else{
-//                     maxi=max(maxi,i-st.top());
-//                 }
+//                 right++;
+//             }
+//             if(left==right){
+//                 maxians=max(maxians,2*right);
+//             }
+//             else if(right>=left){
+//                 left=0;right=0;
 //             }
 //         }
         
-//         return maxi;
+//         left=0;right=0;
         
-        int maxians=0;
-        int left=0,right=0;
+//         for(int i=s.length()-1;i>=0;i--){
+//             if(s[i]=='('){
+//                 left++;
+//             }
+//             else{
+//                 right++;
+//             }
+//             if(left==right){
+//                 maxians=max(maxians,2*left);
+//             }
+//             else if(left>=right){
+//                 left=0;
+//                 right=0;
+//             }
+//         }
         
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='('){
-                left++;
-            }
-            else{
-                right++;
-            }
-            if(left==right){
-                maxians=max(maxians,2*right);
-            }
-            else if(right>=left){
-                left=0;right=0;
-            }
-        }
-        
-        left=0;right=0;
-        
-        for(int i=s.length()-1;i>=0;i--){
-            if(s[i]=='('){
-                left++;
-            }
-            else{
-                right++;
-            }
-            if(left==right){
-                maxians=max(maxians,2*left);
-            }
-            else if(left>=right){
-                left=0;
-                right=0;
-            }
-        }
-        
-        return maxians;
+//         return maxians;
     }
 };
