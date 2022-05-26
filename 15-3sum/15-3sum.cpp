0808df99fa5440e88cf193a4916281bbe399acf7
EkanshGupta-02 <76432963+EkanshGupta-02@users.noreply.github.com>
2022-05-26 22:17:08 +0530

    Time: 163 ms (35.77%), Space: 23.8 MB (21.32%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-         int n = nums.size()-1;
-        set<vector<int>> ans;
-    int r,l;
-    for(int i=0; i<n-1; i++){
-
-            l= i+1;
-            r= n;
+       sort(nums.begin(),nums.end());
+        int n=nums.size();
+        set<vector<int>>temp;
+        
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int l=i+1;
+            int r=n-1;
+            
             while(l<r){
-                int sum = nums[i] + nums[l] + nums[r];
+                int sum=nums[i]+nums[l]+nums[r];
                 if(sum==0){
-                    ans.insert({nums[i], nums[l] , nums[r]});
-                    l++;
+                    temp.insert({nums[i],nums[l],nums[r]});
                     r--;
+                while(l<r && nums[r]==nums[r+1]){
+                        r--;
+                    }
                 }
-                else if (sum>0){
+                if(sum>0){
                     r--;
-
                 }
                 else{
                     l++;
                 }
-
             }
-
+            
         }
-        vector<vector<int>> v(ans.begin(), ans.end());
-        return v;
+        
+        vector<vector<int>>ans(temp.begin(),temp.end());
+        return ans;
     }
 };
