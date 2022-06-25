cade0eae05787bb395227579a83886a6d9e767a2
EkanshGupta-02 <76432963+EkanshGupta-02@users.noreply.github.com>
2022-06-25 22:10:37 +0530

    Time: 5 ms (24.49%), Space: 5.9 MB (23.82%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    int digisum(int n){
-        int sum=0;
-        
-        while(n!=0){
-            sum+=n%10;
-            n=n/10;
-        }
-        
-        return sum;
-    }
     int addDigits(int num) {
         if(num==0){
             return 0;
         }
-        int n=num;
-        
-        while(n>9){
-            n=digisum(n);
+       if(num<10){
+           return num;
+       }
+    if(num%9==0){
+        return 9;
+    }
+        else{
+            return num%9;
         }
-        
-        return n;
     }
 };
