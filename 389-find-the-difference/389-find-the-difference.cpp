8fcd32947b435c8d01d6378a3c81afb25f1b23a1
EkanshGupta-02 <76432963+EkanshGupta-02@users.noreply.github.com>
2022-07-02 21:22:57 +0530

    Time: 3 ms (77.17%), Space: 6.7 MB (67.15%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
+        int i=0;
+        while(s[i]==t[i] && i<s.length()){
+            i++;
+        }
+        
+          return t[i];
+           
+    }
+};
