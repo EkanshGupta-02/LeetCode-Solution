15af9679bceadf9b79bef112fadd147fb215c3a4
EkanshGupta-02 <76432963+EkanshGupta-02@users.noreply.github.com>
2022-08-20 21:25:30 +0530

    Time: 3 ms (54.92%), Space: 7.9 MB (25.38%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    int solve(vector<int>&nums,int index,vector<int>&dp){
+        if(index>=nums.size()){
+            return 0;
+        }
+        
+        if(dp[index]!=-1) return dp[index];
+        
+        int incl = nums[index] + solve(nums,index+2,dp);
+        int excl = solve(nums,index+1,dp);
+        
+        return dp[index] = max(incl,excl);
+    }
+public:
+    int rob(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        return solve(nums,0,dp);
+    }
+};
