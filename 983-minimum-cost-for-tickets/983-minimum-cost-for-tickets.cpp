ab1ad4f06b61457b30381e7af17d67be620cc961
EkanshGupta-02 <76432963+EkanshGupta-02@users.noreply.github.com>
2022-09-01 00:11:59 +0530

    Time: 8 ms (45.22%), Space: 9.7 MB (35.54%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -20,10 +20,33 @@ private:
         
         return dp[index] = min(option1,min(option2,option3));
     }
+    
+    int solvetab(int n,vector<int>&days,vector<int>&costs){
+        vector<int>dp(n+1,INT_MAX);
+        
+        dp[n]=0;
+        
+        for(int k=n-1;k>=0;k--){
+        int option1 = costs[0] + dp[k+1];
+        
+        int i;
+        for(i = k;i<n && days[i] < days[k] + 7;i++);
+        
+        int option2 = costs[1] + dp[i];
+        
+        for(i = k;i<n && days[i] < days[k] + 30;i++);
+        
+        int option3 = costs[2] + dp[i];
+        
+          dp[k] = min(option1,min(option2,option3));
+        }
+        
+        return dp[0];
+    }
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
-        vector<int>dp(n+1,-1);
-        return solve(n,days,costs,0,dp);
+        // vector<int>dp(n+1,-1);
+        return solvetab(n,days,costs);
     }
 };
