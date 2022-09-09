d71771997cb3486b1a8df96e67b84bafdf33fee4
EkanshGupta-02 <76432963+EkanshGupta-02@users.noreply.github.com>
2022-09-09 19:27:31 +0530

    Time: 75 ms (20.56%), Space: 20.8 MB (47.58%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -27,6 +27,23 @@ private:
         
         return dp[0][0];
     }
+    int solvetabSO(vector<int>&arr){
+        int n = arr.size();
+        vector<int>curr(n+1,0);
+        vector<int>next(n+1,0);
+        
+        for(int index=n-1;index>=0;index--){
+            for(int time = index;time>=0;time--){
+          int taken = arr[index]*(time+1) + next[time+1];
+          int nottaken = next[time];
+        
+            curr[time]= max(taken,nottaken);
+            }
+            next=curr;
+        }
+        
+        return next[0];
+    }
     public:
     int maxSatisfaction(vector<int>& satisfaction) {
         // int n = satisfaction.size();
