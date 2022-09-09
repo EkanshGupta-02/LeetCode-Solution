88507984854b6fc7cec86c75dac333aeee6de632
EkanshGupta-02 <76432963+EkanshGupta-02@users.noreply.github.com>
2022-09-09 19:21:18 +0530

    Time: 63 ms (29.59%), Space: 20.9 MB (21.71%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -12,11 +12,27 @@ private:
         
         return dp[time][index]= max(taken,nottaken);
     }
+    int solvetab(vector<int>&arr){
+        int n = arr.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        
+        for(int index=n-1;index>=0;index--){
+            for(int time = index;time>=0;time--){
+          int taken = arr[index]*(time+1) + dp[time+1][index+1];
+          int nottaken = dp[time][index+1];
+        
+             dp[time][index]= max(taken,nottaken);
+            }
+        }
+        
+        return dp[0][0];
+    }
     public:
     int maxSatisfaction(vector<int>& satisfaction) {
-        int n = satisfaction.size();
+        // int n = satisfaction.size();
         sort(satisfaction.begin(),satisfaction.end());
-        vector<vector<int>>dp(n+1,vector<int>(n,-1));
-        return solve(satisfaction,0,0,dp);
+        // vector<vector<int>>dp(n+1,vector<int>(n,-1));
+        // return solve(satisfaction,0,0,dp);
+        return solvetab(satisfaction);
     }
 };
