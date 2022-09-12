47d105c6f0aec91937c64ced706c0c0be1170542
EkanshGupta-02 <76432963+EkanshGupta-02@users.noreply.github.com>
2022-09-12 21:51:05 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (74.97%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int p=0;
-        int q=n-1;
+       int n = nums.size();
+        int s = 0;
+        int e = n-1;
         
-        while(p<=q){
-            int m=p+(q-p)/2;
+        while(s<=e){
+            int mid = s + (e-s)/2;
             
-            if(nums[m]==target){
-                return m;
+            if(nums[mid]==target){
+                return mid;
             }
             
-            if(nums[p]<=nums[m]){
-                if(target>=nums[p] && target<=nums[m]){
-                    q=m-1;
+            if((nums[s]==nums[mid])  && (nums[mid]==nums[e])){
+                s++;
+                e--;
+            }
+            
+            else if(nums[s]<=nums[mid]){
+                if(nums[s]<=target && nums[mid]>target){
+                    e=mid-1;
                 }
                 else{
-                    p=m+1;
+                    s=mid+1;
                 }
             }
-            
             else{
-                if(target>=nums[m] && target<=nums[q]){
-                    p=m+1;
+                 if(nums[mid]< target && nums[e]>=target){
+                    s=mid+1;
                 }
                 else{
-                    q=m-1;
+                    e=mid-1;
                 }
             }
         }
