02794c3dc62a512fb60611e88b507e0d116142dd
EkanshGupta-02 <76432963+EkanshGupta-02@users.noreply.github.com>
2022-09-20 23:21:33 +0530

    Time: 744 ms (18.62%), Space: 109.3 MB (14.62%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -3,9 +3,9 @@ public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        int ans=0;
         vector<vector<int>>dp(nums1.size()+1,vector<int>(nums2.size()+1,0));
         
+        int ans = 0;
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
                 if(nums1[i]==nums2[j]){
@@ -14,7 +14,7 @@ public:
                 ans = max(ans,dp[i][j]);
             }
         }
-                                           
+        
         return ans;
     }
 };
