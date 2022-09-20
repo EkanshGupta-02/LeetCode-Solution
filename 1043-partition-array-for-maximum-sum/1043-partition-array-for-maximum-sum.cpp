e2ccb01c9d344b9892828d52d78863435971b51a
EkanshGupta-02 <76432963+EkanshGupta-02@users.noreply.github.com>
2022-09-20 10:11:15 +0530

    Time: 31 ms (43.53%), Space: 8.7 MB (33.87%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -16,9 +16,27 @@ class Solution {
         
         return dp[idx] = ans;
     }
+    int solvetab(vector<int>&arr,int k){
+        int n = arr.size();
+        vector<int>dp(arr.size()+1,0);
+        
+        for(int idx=n-1;idx>=0;idx--){
+            int ans = -1;
+            int maxi = -1;
+        
+            for(int i = idx;i<arr.size() && i<idx+k;i++){
+                maxi = max(maxi,arr[i]);
+                ans = max(ans,maxi*(i-idx+1) + dp[i+1]);
+            }
+
+            dp[idx] = ans;
+        }
+        return dp[0];
+    }
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        vector<int>dp(arr.size()+1,-1);
-       return solve(arr,k,0,dp);
+       //  vector<int>dp(arr.size()+1,-1);
+       // return solve(arr,k,0,dp);
+        return solvetab(arr,k);
     }
 };
