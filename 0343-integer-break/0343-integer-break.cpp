38dba9a98fa01e26bfe13e145ea8720b66b7e27a
EkanshGupta-02 <76432963+EkanshGupta-02@users.noreply.github.com>
2022-10-20 11:31:30 +0530

    Time: 4 ms (31.59%), Space: 6.8 MB (7.64%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,22 +1,26 @@
 class Solution {
-    int solve(int num,int sum){
+    int solve(int num,int sum,vector<vector<int>>&dp){
         if(num==1){
             return 1;
         }
         if(num<0){
             return 0;
         }
-        int nottaken = solve(num-1,sum);
+        if(dp[num][sum]!=-1){
+            return dp[num][sum];
+        }
+        int nottaken = solve(num-1,sum,dp);
         
         int taken = 0;
         if(sum>=num){ 
-            taken = num*solve(num,sum-num);
+            taken = num*solve(num,sum-num,dp);
         }
         
-        return max(taken,nottaken);
+        return dp[num][sum] = max(taken,nottaken);
     }
 public:
     int integerBreak(int n) {
-        return solve(n-1,n);      
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solve(n-1,n,dp);      
     }
 };
