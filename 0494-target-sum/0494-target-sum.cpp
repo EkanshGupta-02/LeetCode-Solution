efffe52eb4b3b4f8ae289870edb92512bf1221ad
EkanshGupta-02 <76432963+EkanshGupta-02@users.noreply.github.com>
2022-10-20 12:43:54 +0530

    Time: 45 ms (52.06%), Space: 23.2 MB (25.74%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    int solve(vector<int>& nums,int idx,int sum,int target){
+    int solve(vector<int>& nums,int idx,int sum,int target,vector<vector<int>>&dp,int& total){
         if(idx==nums.size()){
             if(sum == target){
                 return 1;
@@ -9,13 +9,17 @@ class Solution {
             }
         }
         
-        int choice1 = solve(nums,idx+1,sum+nums[idx],target);
-        int choice2 = solve(nums,idx+1,sum-nums[idx],target);
+        if(dp[idx][sum+total]!=-1) return dp[idx][sum+total];
         
-        return choice1 + choice2;
+        int choice1 = solve(nums,idx+1,sum+nums[idx],target,dp,total);
+        int choice2 = solve(nums,idx+1,sum-nums[idx],target,dp,total);
+        
+        return dp[idx][sum+total] = choice1 + choice2;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(nums,0,0,target);
+        int total = accumulate(nums.begin(),nums.end(),0);
+        vector<vector<int>>dp(nums.size()+1,vector<int>(2*total+1,-1));
+        return solve(nums,0,0,target,dp,total);
     }
 };
