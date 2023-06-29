ade6bc6213f03a111a2bce134699aaeb784c6a4c
EkanshGupta-02 <76432963+EkanshGupta-02@users.noreply.github.com>
2023-06-29 08:04:07 +0530

    Added solution - LeetHub

--- Majority Element - GFG/majority-element.cpp
+++ Majority Element - GFG/majority-element.cpp
@@ -14,12 +14,34 @@ class Solution{
     // a: input array
     // size: size of input array
     int majorityElement(int a[], int size){
-        unordered_map<int,int>mp;
-        for(int i=0;i<size;i++){
-            mp[a[i]]++;
-            if(mp[a[i]]>size/2){
-                return a[i];
+        int count = 1, res = 0;
+        for (int i = 1; i < size; i++)
+        {
+            if (a[res] == a[i])
+            {
+                count++;
             }
+            else
+            {
+                count--;
+            }
+            if (count == 0)
+            {
+                res = i;
+                count = 1;
+            }
+        }
+        count = 0;
+        for (int i = 0; i < size; i++)
+        {
+            if (a[res] == a[i])
+            {
+                count++;
+            }
+        }
+        if (count > (size / 2))
+        {
+            return a[res];
         }
         return -1;
     }
