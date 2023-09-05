3b1532b28d9be79b521cb80465bf73c117ce657e
EkanshGupta-02 <76432963+EkanshGupta-02@users.noreply.github.com>
2023-09-05 09:36:55 +0530

    Added solution - LeetHub

--- Flattening a Linked List - GFG/flattening-a-linked-list.cpp
+++ Flattening a Linked List - GFG/flattening-a-linked-list.cpp
@@ -111,36 +111,57 @@ struct Node{
 
 /*  Function which returns the  root of 
     the flattened linked list. */
-Node* merge(Node*a ,Node* b){
-    Node* temp = new Node(0);
-    Node *res = temp;
+/*
+ * Definition for linked list.
+ * class Node {
+ *  public:
+ *		int data;
+ *		Node *next;
+ * 		Node *child;
+ *		Node() : data(0), next(nullptr), child(nullptr){};
+ *		Node(int x) : data(x), next(nullptr), child(nullptr) {}
+ *		Node(int x, Node *next, Node *child) : data(x), next(next), child(child) {}
+ * };
+ */
+
+Node* mergeTwoLists(Node* a, Node* b) {
     
-    while(a!=NULL && b!=NULL){
-        if(a->data<b->data){
-            temp->bottom = a;
-            temp= temp->bottom;
-            a = a->bottom;
+    Node *temp = new Node(0);
+    Node *res = temp; 
+    
+    while(a != NULL && b != NULL) {
+        if(a->data < b->data) {
+            temp->bottom = a; 
+            temp = temp->bottom; 
+            a = a->bottom; 
         }
-        else{
+        else {
             temp->bottom = b;
-            temp= temp->bottom;
-            b = b->bottom;
+            temp = temp->bottom; 
+            b = b->bottom; 
         }
     }
     
-    if(a!=NULL) temp->bottom = a;
-    else temp->bottom = b;
+    if(a) temp->bottom = a; 
+    else temp->bottom = b; 
     
-    return res->bottom;
+    return res -> bottom; 
+    
 }
-Node *flatten(Node *root)
+Node* flatten(Node* root) 
 {
-   if(root==NULL || root->next==NULL) return root;
-   
-   root->next = flatten(root->next);
-   
-   root = merge(root,root->next);
-   
-   return root;
+	if (root == NULL || root->next == NULL) 
+            return root; 
+  
+        // recur for list on right 
+        root->next = flatten(root->next); 
+  
+        // now merge 
+        root = mergeTwoLists(root, root->next); 
+  
+        // return the root 
+        // it will be in turn merged with its left 
+        return root; 
 }
+
 
